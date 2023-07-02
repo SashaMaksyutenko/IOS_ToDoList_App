903465f4f184f3d9ce44ed01d6cc24127614211e
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-07-02 19:54:43 +0300

    Starting make ToDoListAppfor IOS

--- ToDoList/Other/ToDoListApp.swift
+++ ToDoList/Other/ToDoListApp.swift
@@ -4,11 +4,14 @@
 //
 //  Created by Sasha Maksyutenko on 02.07.2023.
 //
-
+import FirebaseCore
 import SwiftUI
 
 @main
 struct ToDoListApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
