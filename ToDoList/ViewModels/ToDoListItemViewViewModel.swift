9e2e37e939d934344f5e9a216b7c79658d156ac8
SashaMaksyutenko <sasha.maksyutenko@gmail.com>
2023-07-10 14:47:26 +0300

    Profile and Log Out

--- ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -4,10 +4,24 @@
 //
 //  Created by Sasha Maksyutenko on 03.07.2023.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 /// ViewModel for single of item view (each row in items list)
 /// Primary tab
 class ToDoListItemViewViewModel:ObservableObject{
     init(){}
+    func toggleIsDone(item:ToDoListItem){
+        var itemCopy=item
+        itemCopy.setDone(!item.isDone)
+        guard let uid=Auth.auth().currentUser?.uid else{
+            return
+        }
+        let db=Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -12,13 +12,63 @@ struct ProfileView: View {
     var body: some View {
         NavigationView{
             VStack{
+                if let user=viewModel.user{
+                    profile(user:user)
+                }
+                    else{
+                        Text("Loading Profile...")
+                    }
+                }
+                    .navigationTitle("Profile")
             }
-            .navigationTitle("Profile")
+        .onAppear{
+            viewModel.fetchUser()
+        }
+        }
+    @ViewBuilder
+    func profile(user:User)-> some View{
+        // avatar
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125,height: 125)
+            .padding()
+        //                // info: name, email, member since
+        VStack(alignment: .leading){
+            HStack{
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            HStack{
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            HStack{
+                Text("Member Since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        .padding()
+        // sign out
+        Button("Log Out") {
+            viewModel.logOut()
+        }
+        .tint(.red)
+        //padding()
+        Spacer()
+        
+    }
+    }
+    struct ProfileView_Previews: PreviewProvider {
+        static var previews: some View {
+            ProfileView()
         }
     }
-}
-struct ProfileView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProfileView()
-    }
-}
+
